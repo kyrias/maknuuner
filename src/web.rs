d6a7f5e98262e73eaeeb1fcc1af127f4906f7209
Johannes Löthberg <me@remmy.foo>
2026-08-04 17:49:29 +0200

    Split styles into new web::styles module

--- src/web.rs
+++ src/web.rs
@@ -1,8 +1,7 @@
 use anyhow::Context as _;
 use topcoat::{
     Result,
-    asset::{AssetBundle, RouterBuilderAssetExt as _, asset},
-    font::{Font, font},
+    asset::{AssetBundle, RouterBuilderAssetExt as _},
     router::{Router, RouterBuilderDiscoverExt as _, error::redirect, layout, page},
     view::view,
 };
@@ -10,35 +9,8 @@ use topcoat::{
 use crate::lexicon::Lexicon;
 
 mod search;
+mod styles;
 mod title;
-
-const AMIRI: Font = font! {
-    "Amiri",
-    @font-face {
-        src: url(asset!("assets/Amiri-1.003/Amiri-Regular.ttf")) format("truetype");
-        font-style: normal;
-        font-weight: normal;
-        font-display: swap;
-    }
-    @font-face {
-        src: url(asset!("assets/Amiri-1.003/Amiri-Italic.ttf")) format("truetype");
-        font-style: italic;
-        font-weight: normal;
-        font-display: swap;
-    }
-    @font-face {
-        src: url(asset!("assets/Amiri-1.003/Amiri-Bold.ttf")) format("truetype");
-        font-style: normal;
-        font-weight: bold;
-        font-display: swap;
-    }
-    @font-face {
-        src: url(asset!("assets/Amiri-1.003/Amiri-BoldItalic.ttf")) format("truetype");
-        font-style: italic;
-        font-weight: bold;
-        font-display: swap;
-    }
-};
 
 pub(super) async fn start(lexicon: Lexicon) -> anyhow::Result<()> {
     topcoat::start(
@@ -61,105 +33,11 @@ async fn root_layout(slot: Result) -> Result {
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 title::title()
+
                 topcoat::dev::script()
                 topcoat::runtime::script()
-                topcoat::font::link(font: AMIRI)
-                <style>
-                    "
-*, *::before, *::after { box-sizing: border-box; }
-* { margin: 0; padding: 0; }
-input, button, textarea, select {
-  font: inherit;
-}
 
-:root {
-    font-family: "
-                    (AMIRI.family())
-                    ", serif;
-    font-size: 14pt;
-    line-height: 1.85;
-    text-wrap: balance;
-}
-
-:lang(ar) {
-    font-size: 140%;
-}
-
-:lang(en) {
-}
-
-html {
-    padding-left: 0.5em;
-    padding-right: 0.5em;
-}
-
-input {
-    padding-left: 0.3em;
-    padding-right: 0.3em;
-}
-
-ol {
-    padding-left: 2em;
-}
-"
-                    (topcoat::view::Unescaped::new_unchecked(
-                        "
-
-input.search {
-    margin-top: 0.5em;
-    width: 20em;
-}
-
-.results {
-    margin-top: 1em;
-
-    & .inner {
-        padding-left: 0.75em;
-    }
-}
-
-.result {
-    &:not(:first-child) {
-        margin-top: 0.75em;
-    }
-
-    h3 {
-        & .root {
-            font-size: 0.9em;
-        }
-        & .lemma {
-            margin-left: 0.5em;
-        }
-
-        & a {
-            margin-left: 0.75em;
-
-            font-size: 1em;
-            color: oklch(0.5999 0 0 / 40%);
-            transition: color 0.2s;
-
-            text-decoration: none;
-        }
-        &:hover a {
-                color: oklch(0.469 0.224 321.186);
-        };
-    }
-
-    h4 {
-        margin-top: 0.5em;
-    }
-
-    .pos {
-        margin-left: 0.75em;
-    }
-    .transcription {
-        margin-left: 0.75em;
-        color: oklch(0.5 0 0);
-    }
-}
-",
-                    ))
-                </style>
+                styles::styles()
             </head>
             <body>
                 <header><h1>"Maknuuner"</h1></header>
---
+++ src/web/styles.rs
@@ -0,0 +1,162 @@
+use topcoat::{
+    Result,
+    asset::asset,
+    font::{Font, font},
+    view::{component, view},
+};
+
+const AMIRI: Font = font! {
+    "Amiri",
+    @font-face {
+        src: url(asset!("assets/Amiri-1.003/Amiri-Regular.ttf")) format("truetype");
+        font-style: normal;
+        font-weight: normal;
+        font-display: swap;
+    }
+    @font-face {
+        src: url(asset!("assets/Amiri-1.003/Amiri-Italic.ttf")) format("truetype");
+        font-style: italic;
+        font-weight: normal;
+        font-display: swap;
+    }
+    @font-face {
+        src: url(asset!("assets/Amiri-1.003/Amiri-Bold.ttf")) format("truetype");
+        font-style: normal;
+        font-weight: bold;
+        font-display: swap;
+    }
+    @font-face {
+        src: url(asset!("assets/Amiri-1.003/Amiri-BoldItalic.ttf")) format("truetype");
+        font-style: italic;
+        font-weight: bold;
+        font-display: swap;
+    }
+};
+
+#[component]
+pub(super) async fn styles() -> Result {
+    view! {
+        topcoat::font::link(font: AMIRI)
+        reset_styles()
+        common_styles()
+        page_styles()
+    }
+}
+
+#[component]
+async fn reset_styles() -> Result {
+    let s = "
+*, *::before, *::after {
+    box-sizing: border-box;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+input, button, textarea, select {
+  font: inherit;
+}
+";
+    view! { <style>(s)</style> }
+}
+
+#[component]
+async fn common_styles() -> Result {
+    view! {
+        <style>
+            "
+:root {
+    font-family: "
+            (AMIRI.family())
+            ", serif;
+    font-size: 14pt;
+    line-height: 1.85;
+    text-wrap: balance;
+}
+
+:lang(ar) {
+    font-size: 140%;
+}
+
+:lang(en) {
+}
+
+html {
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+}
+
+input {
+    padding-left: 0.3em;
+    padding-right: 0.3em;
+}
+
+ol {
+    padding-left: 2em;
+}
+"
+        </style>
+    }
+}
+
+// TODO: It would probably make sense to handle per-page styles in the same way as `web::title`.
+#[component]
+async fn page_styles() -> Result {
+    let s = "
+input.search {
+    margin-top: 0.5em;
+    width: 20em;
+}
+
+.results {
+    margin-top: 1em;
+
+    & .inner {
+        padding-left: 0.75em;
+    }
+}
+
+.result {
+    &:not(:first-child) {
+        margin-top: 0.75em;
+    }
+
+    h3 {
+        & .root {
+            font-size: 0.9em;
+        }
+        & .lemma {
+            margin-left: 0.5em;
+        }
+
+        & a {
+            margin-left: 0.75em;
+
+            font-size: 1em;
+            color: oklch(0.5999 0 0 / 40%);
+            transition: color 0.2s;
+
+            text-decoration: none;
+        }
+        &:hover a {
+                color: oklch(0.469 0.224 321.186);
+        };
+    }
+
+    h4 {
+        margin-top: 0.5em;
+    }
+
+    .pos {
+        margin-left: 0.75em;
+    }
+    .transcription {
+        margin-left: 0.75em;
+        color: oklch(0.5 0 0);
+    }
+}
+";
+    view! { <style>(topcoat::view::Unescaped::new_unchecked(s))</style> }
+}
