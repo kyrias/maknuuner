17321f8304db41514f025469522f80526a7bc702
Johannes Löthberg <me@remmy.foo>
2026-08-07 02:37:12 +0200

    Delete WIP prepare.bash script

--- prepare.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-mkdir -p assets/
-
-curl -Lo assets/Amiri-1.003.zip https://github.com/aliftype/amiri/releases/download/1.003/Amiri-1.003.zip
-unzip assets/Amiri-1.003.zip -d assets/
